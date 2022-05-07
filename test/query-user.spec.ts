ae8b1f71f7460edf1d515509a6dbfd96e4b0e9de
Jose Quesada <josesk8mqc@gmail.com>
2022-05-07 18:06:22 -0400

    modify query-user test

--- test/query-user.spec.ts
+++ test/query-user.spec.ts
@@ -1,10 +1,11 @@
 import { expect } from "chai";
 import { queryUser } from "./test-apis";
-import { models } from "../src/models";
+import { connectDb, models } from "../src/models";
 import { UserInterface } from "../src/interfaces";
 
 describe("end to end query user by user_id", () => {
     before(async () => {
+        await connectDb();
         await models.User.deleteMany({});
         const userData: UserInterface = {
             user_id: "test-user_id",
@@ -17,6 +18,12 @@ describe("end to end query user by user_id", () => {
         const userTest = new models.User(userData);
         await userTest.save();
     });
+
+    it("should return an error if user_id is not in the db", async () => {
+        const result = await queryUser({userId : "test-user_id_incorrect"});
+        expect(result.data).to.have.property("errors");
+        expect(result.data.errors[0].message).to.be.equal("There is not a user with this user_id in the database"); 
+    })
 
     it("should return the user info that correpond to the user_id", async () => {
         const result = await queryUser({userId : "test-user_id"});
